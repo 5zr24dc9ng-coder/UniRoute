1ec50713a77f5c087f2ffe1790a35070c81c67c5
塚田開 <kaitsukada@tsukadakainoMacBook-Air.local>
2026-06-23 15:17:28 +0900

    Viteプラグインをoxcに移行してWarning解消

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-oxc'; // ← ここを新しいプラグインに変更
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 });
